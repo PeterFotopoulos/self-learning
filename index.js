4d695c18b8ff890b7e5b6999ac7e849b17ea705f
Peter Fotopoulos <pfot0578@uni.sydney.edu.au>
2025-03-31 21:53:12 +1100

    crons jobs

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   return NextResponse.json({ ok: true });
 }
+
+if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).end('Unauthorized');
+  }
+
+  
